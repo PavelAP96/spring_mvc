3ce0b9ca4da532720c8c98172c1bee3931dd9a82
pavelpahomkin <pakhomkin96@mail.ru>
2022-10-19 16:27:17 +0300

    Java Code Configuration

---
+++ src/main/java/ru/ppakhomkin/udemy/confguration/DatabaseConfig.java
@@ -0,0 +1,49 @@
+package ru.ppakhomkin.udemy.confguration;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        try {
+            comboPooledDataSource.setDriverClass("org.postgresql.Driver");
+            comboPooledDataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/udemy_spring_course");
+        } catch (PropertyVetoException e) {
+            throw new RuntimeException(e);
+        }
+        return comboPooledDataSource;
+    }
+
+    @Bean
+    public SessionFactory sessionFactory() {
+        LocalSessionFactoryBean localSessionFactoryBean = new LocalSessionFactoryBean();
+        localSessionFactoryBean.setDataSource(dataSource());
+        localSessionFactoryBean.setPackagesToScan("ru.ppakhomkin.udemy");
+        Properties hibernateProperties = new Properties();
+        hibernateProperties.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        hibernateProperties.setProperty("hibernate.show_sql", "true");
+        localSessionFactoryBean.setHibernateProperties(hibernateProperties);
+        return localSessionFactoryBean.getObject();
+    }
+
+    @Bean
+    public HibernateTransactionManager transactionManager() {
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(sessionFactory());
+        return hibernateTransactionManager;
+    }
+}
